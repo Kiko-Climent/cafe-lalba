8692d53e778b6d22ba7566e00b93146e27fda6e6
Kiko Climent <climent.kiko@gmail.com>
2023-12-03 19:37:53 +0000

    delete auth app, use the one from django

--- lalba/settings.py
+++ lalba/settings.py
@@ -45,8 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
-    'authentication',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
@@ -132,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudnary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
