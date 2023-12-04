60f9e2177ff419e4c13d2fa2d21531d4b1594db2
Kiko Climent <climent.kiko@gmail.com>
2023-12-04 11:42:07 +0000

    create signup view

--- lalba/settings.py
+++ lalba/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
+    'reservations'
 ]
 
 MIDDLEWARE = [
