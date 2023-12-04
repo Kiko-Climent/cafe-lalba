a7abd6533e8b6e04ff0ccc0db3eb8577d2d947e9
Kiko Climent <climent.kiko@gmail.com>
2023-12-04 13:48:10 +0000

    create more views in resercations app

--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -17,10 +18,20 @@ def signup(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username=request.POST['username'],
+                user = User.objects.create_user(username=request.POST['username'], 
                 password=request.POST['password1'])
                 user.save()
-                return HttpResponse ('User created succesfully')
-            except:
-                return HttpResponse('Username already in data base')
-        return HttpResponse('Password do not match')
+                login(request, user)
+                return redirect('reservations')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                'error': 'User already exists'
+            })      
+        return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                'error': 'Password do not match'
+        })
+
+def reservations(request):
+    return render(request, 'reservations.html')
