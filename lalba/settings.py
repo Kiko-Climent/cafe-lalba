96fb6545bd657f48d0ca6f2c82f2acf714311d36
Kiko Climent <climent.kiko@gmail.com>
2023-12-16 12:43:29 +0000

    Style Sign Up section

--- lalba/settings.py
+++ lalba/settings.py
@@ -59,6 +59,9 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+ACCOUNT_SIGNUP_REDIRECT_URL = '/'  # Esta línea redirigirá a la página de inicio
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
